551177ae1a52322104c47b84f51294120a9d14a7
Nicholas Jarin <nicholasjarin@gmail.com>
2017-01-24 12:32:07 -0800

    update question 3 and 4 in hamster

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -13,15 +13,25 @@ else puts "Name the hamster"
 end
 
 # How loud is hamster?
+	# Give it on a scale of 1-10
+	# If not an integer, ask for an integer
 puts "Listen to the hamster. On a scale of 1-10, how loud is it?"
-until loudness=Integer(gets.chomp) rescue false
-	if loudness 
- 	   p loudness 
-	else puts "I'm sorry, give me a number on a scale of 1-10"
-	end
+loudness = gets.chomp
+# VERIFY INPUT IS A NUMBER 1-10
+until loudness.to_i == 1 or loudness.to_i == 2 or loudness.to_i == 3 or loudness.to_i == 4 or loudness.to_i == 5 or loudness.to_i == 6 or loudness.to_i == 7 or loudness.to_i == 8 or loudness.to_i == 9 or loudness.to_i == 10
+	puts puts "I'm sorry, give me a number on a scale of 1-10"
+	loudness = gets.chomp
 end
 
 # What fur color? 
+puts "Is the fur color black, white, brown, orange, or mixed?"
+fur_color = gets.chomp
+until fur_color == "black" or fur_color == "white" or fur_color == "brown" or fur_color == "orange" or fur_color == "mixed" 
+	puts "I'm sorry, give me one of the colors I listed as options"	
+	fur_color = gets.chomp	
+end
+
+
 
 # Is this a good candidate for adoption?
 
