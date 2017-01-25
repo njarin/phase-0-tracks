3fd6021dc5906b3b884706bdcbdfb71024608c40
Nicholas Jarin <nicholasjarin@gmail.com>
2017-01-24 21:02:07 -0800

    update outcomes for vampire questionnaire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -52,8 +52,6 @@ if insurance.downcase == "yes"
 else insurance = false
 end
 
-
-# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 # Does age match birth year?
 age_math = (age - (2017 - birth_year))
 if age_math == 0
@@ -61,14 +59,28 @@ if age_math == 0
 else age_math = false
 end
 
-vamp_identity = "Results inconclusive on whether #{name} is a vampire."
+# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+vamp_identity = "Results are inconclusive on whether #{name} is a vampire."
 if age_math && (garlic_bread || insurance)
 	vamp_identity = "#{name} is probably not a vampire"
 end
 
+# If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
 
+if (age_math == false) && ((garlic_bread == false) || (insurance == false))
+	vamp_identity = "#{name} is probably a vampire."
+end
 
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
 
+if (age_math == false) && (garlic_bread == false) && (insurance == false)
+	vamp_identity = "#{name} is almost certainly a vampire."
+end
 
+# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+
+if (name == "Drake Cula") || (name == "Tu Fang")
+	vamp_identity = "#{name} is definitely a vampire."
+end
 
 puts vamp_identity
