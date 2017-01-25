fc71ddb7212f9eb458e40e9ddb9c7186568ed94b
Nicholas Jarin <nicholasjarin@gmail.com>
2017-01-24 18:18:40 -0800

    add questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+# Get a name
+puts "What is your name?"
+name = gets.chomp
+
+# Get an age and year of birth
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+
+# Ask if this person likes garlic bread
+puts "Our cafeteria has garlic bread. Would you like to eat some right now? (yes/no)"
+garlic_bread = gets.chomp
+
+# Ask if they want health insurance
+puts "We have a great healthcare plan. Should we sign you up for health insurance?"
+insurance = gets.chomp
