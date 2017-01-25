5070c66f6da157ba1131fdf0361c4cf43c801657
Nicholas Jarin <nicholasjarin@gmail.com>
2017-01-25 14:10:46 -0800

    add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,7 +1,4 @@
 # Encrypt every letter by changing each character into the next one in the alphabet
-# Assign a string variable
-# Index the string
-# Apply .next to each indexed character
 
 # Define a method to encrypt a string
 # .next every character in the string
@@ -10,7 +7,7 @@
 # index the word and then apply .next to every indexed character
 
 
-def encrypt (string1)
+def encrypt(string1)
 	index = 0
 	while index < string1.length
 		string1[index] = string1[index].next
@@ -19,5 +16,25 @@ def encrypt (string1)
 	puts string1
 end
 
-encrypt ("password")
+encrypt("password")
 
+
+# Decryption
+
+# Figure out where in the alphabet a letter is located
+# Subtract 1 from its index location
+# Convert index location to alphabet
+# Print result
+
+def decrypt(string2)
+	alpha = "abcdefghijklmnopqrstuvwxyz"
+	index = 0
+	while index < string2.length
+		decrypt_letter = alpha[(alpha.index(string2[index]) - 1)]
+		string2[index] = decrypt_letter
+		index += 1
+	end
+	puts string2
+end
+
+decrypt("pssword")
