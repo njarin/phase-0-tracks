6b5da67e93dcfd98d351b65b0034951839e75215
Nicholas Jarin <nicholasjarin@gmail.com>
2017-01-31 18:25:18 -0800

    add first successful user input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,2 +1,13 @@
 # 5.2 HASHES RELEASE 3
 
+design_profile = {
+	client_name:"",
+	client_age:"",
+
+}
+
+
+puts "What is the client's age?"
+age = gets.chomp
+design_profile[:client_age] << age
+p design_profile[:client_age]
