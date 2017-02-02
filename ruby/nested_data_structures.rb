27b0bfe21f258e69ad819d104b7d8b32c0a4dab5
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-02 15:51:33 -0800

    create data structure with arrays in hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+# NESTED DATA STRUCTURES
+
+# Create a nested data structure using at least one hash and array 
+
+# Create a data structure that would store the foreign nations that friends have visited.
+
+well_traveled_friends = {
+	Rosin: ['Philippines', 'France', 'England', 'Italy', 'Wales', 'Scotland'],
+	Samantha: ['Japan', 'China', 'Germany', 'Hungary', 'Italy'], 
+	Dylan: ['Japan', 'China', 'Germany', 'Hungary', 'Italy'],
+	Erick: ['China', 'Thailand'],
+	Andrew: ['Costa Rica', 'Belize'],
+	Zack: ['Philippines', 'Canada']
+}
+
+
+# Demonstrate access to different places in the structure using different methods
