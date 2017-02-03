ed07286a685569a331af3c3c968adf5db1190184
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-02 16:29:31 -0800

    add pseudocode for alias generator

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+# SOLO CHALLENGE 5.5
+
+
+# ALIAS CREATOR
+# Swap the first and last name.
+# Change all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', 
+# and all of the consonants (everything else besides the vowels) to the next 
+# consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', 
+# and 'd' would become 'f'.
+
+agent_name = 
+
+agent_name = 
