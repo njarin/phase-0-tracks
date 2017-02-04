1341476e89b9adfb83c2943da9ca54c264af193c
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-03 19:56:34 -0800

    add release 0 code so far

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,2 +1,29 @@
 # PAIRING 5.6 
 
+#Practicing Data Structures!
+
+def search_array(input_array, x)
+  input_array.each {
+  |number| if number == x
+    p x.index
+  end
+    #if input_array[-1] == x
+    #   p input_array[-1]
+    # else p "It's not there."
+    # end
+  }
+end
+
+
+
+
+#   loop do 
+#     p input_array.delete(input_array.last)
+#   break if input_array.last == x
+#     (input_array.length) - 1
+#   end
+# end
+
+array = [7, 23, 56, 19]
+
+p search_array(array, 7)
