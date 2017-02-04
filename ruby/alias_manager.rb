38babb05754659ad16d01f896c8f44ee8526aa93
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-03 16:51:09 -0800

    add vowel changer

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,7 +8,7 @@
 # consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', 
 # and 'd' would become 'f'.
 
-agent_name = "Felicia Torres"
+agent_name = "Felicia Torres William Wallace Zazzy"
 
 # Swap first and last name
 # Turn string into array of first and last name
@@ -18,34 +18,35 @@ agent_alias = agent_name.downcase
 agent_alias = agent_alias.split(' ').reverse
 agent_alias = agent_alias.join(' ')
 agent_alias = agent_alias.split('')
-p agent_name
-p agent_alias
-puts
-# puts
 
 
 # Change vowels in alias to the next vowel in 'aeiou'
 # Change consonants to the next one
-agent_alias.map! {
-	|letter|
-	letter.next
-	}
+# Control for edge cases at "tuvw" and "z" in alphabet 
 
-# Control for consonants that turned into vowels and force them to skip that vowel. 
-agent_alias = agent_alias.join
-agent_alias.gsub!(/[aeiou]/, 'a' => 'b', 'e' => 'f', 'i' => 'j', 'o' => 'p', 'u' => 'v')
+agent_alias.each do |vowel|
+	vowel.gsub!(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+end
 
+agent_alias.each do |consonant|
+	consonant.gsub!(/[dhnt]/, consonant.next.next)
+	consonant.gsub!(/[bcfgjklmpqrs]/, consonant.next)
+	consonant.gsub!(/[wxyz]/, consonant.next)
+	# letter.gsub!(/[z]/, 'z' => 'b')
+end
+
+
+# Capitalize the result like a name
+agent_alias = agent_alias.join('')
+agent_alias = agent_alias.split(' ').map(&:capitalize).join(' ')
+
+# agent_alias.map! {
+# 	|letter|
+# 	letter.next
+# 	}
 
 p agent_alias
-puts
-
-agent_alias.gsub!(/[bfjpv!]/, 'b' => 'e', 'f' => 'i', 'j' => 'o', 'p' => 'u', 'v' => 'a', '!' => ' ')
-
-p agent_alias
-puts
 
 
-# Correctly capitalize the new alias
-# p agent_alias.split(' ').capitalize.join(' ')
 
 
