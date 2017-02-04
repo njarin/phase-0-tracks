c5b615a5970355170054ed028e4b7e17cb18de64
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-03 17:53:43 -0800

    start work on user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,44 +8,43 @@
 # consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', 
 # and 'd' would become 'f'.
 
-agent_name = "Felicia Torres William Wallace Zazzy"
-
 # Swap first and last name
 # Turn string into array of first and last name
 # Reverse array
 # Add name back together into a string
-agent_alias = agent_name.downcase
-agent_alias = agent_alias.split(' ').reverse
-agent_alias = agent_alias.join(' ')
-agent_alias = agent_alias.split('')
+def alias_generator(agent_name)
+	agent_alias = agent_name.downcase
+	agent_alias = agent_alias.split(' ').reverse
+	agent_alias = agent_alias.join(' ')
+	agent_alias = agent_alias.split('')
 
+	# Change vowels in alias to the next vowel in 'aeiou'
+	# Change consonants to the next one
+	# Control for edge cases at "stuvw" and "z" in alphabet 
 
-# Change vowels in alias to the next vowel in 'aeiou'
-# Change consonants to the next one
-# Control for edge cases at "tuvw" and "z" in alphabet 
+	agent_alias.map! do |vowel|
+		vowel.gsub(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+	end
 
-agent_alias.each do |vowel|
-	vowel.gsub!(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+	agent_alias.each do |consonant|
+		consonant.gsub!(/[dhnt]/, consonant.next.next)
+		consonant.gsub!(/[bcfgjklmpqrswxy]/, consonant.next)
+		consonant.gsub!(/[z]/, 'z' => 'b')
+	end
+
+	# Capitalize the result like a name
+	agent_alias = agent_alias.join('')
+	agent_alias = agent_alias.split(' ').map(&:capitalize).join(' ')
 end
 
-agent_alias.each do |consonant|
-	consonant.gsub!(/[dhnt]/, consonant.next.next)
-	consonant.gsub!(/[bcfgjklmpqrs]/, consonant.next)
-	consonant.gsub!(/[wxyz]/, consonant.next)
-	# letter.gsub!(/[z]/, 'z' => 'b')
-end
+#USER INTERFACE
+
+puts "Please type in name of agent"
+agent_name = gets.chomp
+
+puts alias_generator(agent_name)
 
 
-# Capitalize the result like a name
-agent_alias = agent_alias.join('')
-agent_alias = agent_alias.split(' ').map(&:capitalize).join(' ')
-
-# agent_alias.map! {
-# 	|letter|
-# 	letter.next
-# 	}
-
-p agent_alias
 
 
 
