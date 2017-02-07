b28c077809eb6ee207a98cf36cb8c9c6e542e595
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-07 12:16:43 -0800

    add description of puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+# PUPPY CLASS
+
+SPECIES ---------------------
+Canis lupus familiaris
+
+CHARACTERISTICS -------------
+Vertebrate: Yes
+Mammal: Yes
+Legs: 4
+Tail length: Varies
+Fur pattern: Varies
+Fur color: Varies
+Age: Under 1 year old
+
+BEHAVIOR -------------------- 
+Run
+Jump
+Catch
+Play
+Chew
+Eat
+Bark
+Howl
+Wrestle
