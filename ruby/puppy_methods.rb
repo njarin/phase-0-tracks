6ef78d88149e6d879e3b1532c246e17d278077e2
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-07 14:23:39 -0800

    add driver code and mmethods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,6 +7,32 @@ class Puppy
     toy
   end
 
+  def speak(num_times)
+  	num_times.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	dog_age = human_years * 7
+  	dog_age
+  end
+
+  def sit(dog_name)
+  	puts "#{dog_name} is sitting like a dog" 
+  end
+
 end
 
+goober = Puppy.new
+goober.fetch("ball")
 
+goober.speak(5)
+
+goober.roll_over
+
+p goober.dog_years(4)
+
+goober.sit("Goober")
