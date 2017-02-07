64ac13d10fdda36445d7d1f0bbcf1ae116efdef0
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-07 14:50:26 -0800

    update with final release class and instances

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,8 +28,27 @@ class Puppy
   	puts "Initializing new puppy instance..."
   end
 
-
 end
+
+class HockeyPlayer
+	
+	def initialize
+		puts "You've created a new hockey player"
+	end
+
+	def shoot(location)
+		if location == "Upper Right"
+			puts "You scored!"
+		else
+			puts "Big save by the goaltender."
+		end
+	end
+
+	def check(checker_name, target_name)
+		puts "#{checker_name} checked #{target_name}"
+	end
+end
+
 
 goober = Puppy.new
 goober.fetch("ball")
@@ -41,3 +60,22 @@ goober.roll_over
 p goober.dog_years(4)
 
 goober.sit("Goober")
+
+num_players = 0
+hockey_team = []
+
+until num_players == 50
+	hockey_team[num_players] = HockeyPlayer.new
+	num_players += 1
+end
+
+
+mario_lemieux = HockeyPlayer.new
+mario_lemieux.shoot("Upper Right")
+mario_lemieux.shoot("Center")
+mario_lemieux.check("Mario Lemieux", "Joe Sakic")
+
+hockey_team.each { |player| 
+	player.shoot("Upper Right")
+	player.check("Our guy", "Their guy")
+}
