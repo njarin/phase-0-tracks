c9eadd127ca0695c10a0252ff259eb49ca029a31
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-08 15:19:10 -0800

    add pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,44 @@
+# PAIRING GPS 2.2
+
+# ELECTRONIC GROCERY LIST
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # turn string into an array
+  	# use the split(' ') method to do that
+  # set default quantity
+  	# iterate through the array to set grocery item as key, and default quantity as value
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: set item name to a key in the hash and the quantity to the value
+	# inputting that as an array into the hash
+# output: hash
+
+# Method to remove an item from the list
+# input: list, item name
+# steps: delete the key and value that corresponds to the key 
+# output: hash
+
+# Method to update the quantity of an item
+# input: list, item name, new quantity
+# steps: update the hash value with the new quantity
+# output: hash with updated quantity
+
+# Method to print a list and make it look pretty
+# input: list
+# steps: user interface print message with values in front keys
+# output: printed, pretty list
+
+
+
+
+
+
+
+
+
+
