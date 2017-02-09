6908a75ee32e9203ff955bf52a2c359d431bc215
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-08 21:49:20 -0800

    change shout into a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,34 @@
 # 6.4 MODULES
 
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily
-  	"Woooooooooooo!!!"
-  end
+#   def self.yelling_happily
+#   	"Woooooooooooo!!!"
+#   end
+# end
+
+# Shout.yell_angrily("Dang")
+# Shout.yelling_happily
+
+module Shout
+	def fire_breath
+		"Yol Toor Shul!"
+		"Whoa! Fire breath!"
+	end
+
+	def slow_time
+		"Tiid Klo Ul!"
+		"Time ... slowed ... doooooown"
+	end
 end
 
-Shout.yell_angrily("Dang")
-Shout.yelling_happily
+class Dohvahkiin
+	include Shout
+end
+
+class Dragon
+	include Shout
+end
