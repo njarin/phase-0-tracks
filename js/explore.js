ee7f7eb9601af5b1d706de2ba91c5218fc1604cf
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-15 12:39:43 -0800

    add function for reversing strings

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,23 @@
 // Module 7.1 Syntax
+
+// Write a function that will reverse a word
+// Pseudocode 
+// 	Accept a word (string) as a parameter
+// 	Break that word down into characters
+// 	Create a new word with the characters from the old one starting from the end
+// 	Use indexes to take each letter from take from the end (strings have a #length in JS)
+
+var str = "hydrophobic";
+var reversedWord = "";
+
+for (var i = (str.length - 1); i >= 0; i-=1) {
+	reversedWord = (reversedWord + str[i]);
+}
+
+
+// Print the reversedWord given that some condition is true
+console.log(reversedWord);
+if (1 <= 100) {
+	console.log(str);
+	console.log(reversedWord);
+}
