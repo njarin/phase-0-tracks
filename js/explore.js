4456bfa4398b9bcbad920eee2c9c69a8a0298514
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-15 12:51:13 -0800

    refactor code into a function

--- js/explore.js
+++ js/explore.js
@@ -7,17 +7,23 @@
 // 	Create a new word with the characters from the old one starting from the end
 // 	Use indexes to take each letter from take from the end (strings have a #length in JS)
 
-var str = "hydrophobic";
-var reversedWord = "";
+// var string = "hello";
 
-for (var i = (str.length - 1); i >= 0; i-=1) {
-	reversedWord = (reversedWord + str[i]);
+function reverse(string) {
+	var reversedWord = "";
+	for (var i = (string.length - 1); i >= 0; i-=1) {
+		reversedWord = (reversedWord + string[i]);
+		reversedWord;
+	}
+	console.log(reversedWord);
 }
+
+(reverse("hello"))
 
 
 // Print the reversedWord given that some condition is true
-console.log(reversedWord);
-if (1 <= 100) {
-	console.log(str);
-	console.log(reversedWord);
-}
+// console.log(reversedWord);
+// if (1 <= 100) {
+// 	console.log(string);
+// 	console.log(reversedWord);
+// }
