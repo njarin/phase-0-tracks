2ccda38e6fd7b94e5890faebd079cb3e4dc82d42
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-26 14:14:36 -0800

    add project outline, pseudocode

--- databases/birthdays/birthday_tracker.rb
+++ databases/birthdays/birthday_tracker.rb
@@ -1,2 +1,33 @@
 # BIRTHDAY TRACKER
 
+# Create a Ruby program that helps my grandparents keep track of their large family's birthdays
+# Features:
+	# Maintain a database of family members' birthdays
+	# User can add a new family member's birthday to the database 
+	# User can view birthdays in a user-friendly printout
+	# This could be a piece of a larger project that includes a database that groups
+		# family members together in a similar way to a family tree (junction table?)
+
+# PSEUDOCODE 
+
+# Create a family birthday database with some initial values
+# input:
+# steps:
+	# require sqlite3
+	# create a database
+	# store the database in a variable
+	# add some initial hard-coded inputs to the database
+		# could use the oldest family members' birthdays 
+# output: database with some data in it
+
+# Method for adding a new family member to birthday tracking database
+# input: database, values
+# steps:
+
+# output: 
+
+# Method for printing birthdays in a user-friendly way
+# input: database
+# steps: 
+
+# output: printed message
