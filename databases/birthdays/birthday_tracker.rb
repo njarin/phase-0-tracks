78b5af805701f0ac7338efdc0e286ac465d36f40
Nicholas Jarin <nicholasjarin@gmail.com>
2017-02-26 15:05:07 -0800

    create database for birthdays

--- databases/birthdays/birthday_tracker.rb
+++ databases/birthdays/birthday_tracker.rb
@@ -39,15 +39,19 @@
 require 'sqlite3'
 
 family_birthdays = SQLite3::Database.new("birthdays.db")
-birthdays.results_as_hash = true
+family_birthdays.results_as_hash = true
 
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS birthdays(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    birthday VARCHAR(255)
-  )
-SQL
+# create_table_cmd = <<-SQL
+#   CREATE TABLE IF NOT EXISTS birthdays(
+#     id INTEGER PRIMARY KEY,
+#     name VARCHAR(255),
+#     birthday VARCHAR(255)
+#   )
+# SQL
+
+# initial values
+
+
 
 
 # USER INTERFACE
